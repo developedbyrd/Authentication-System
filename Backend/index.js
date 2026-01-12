15987f5f36c3e7878e92fa052ce505300459dab5
developedbyrd <developedbyrd@gmail.com>
2026-01-12 21:50:47 +0530

    Update CORS configuration to allow requests from the production authentication system URL

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://authentication-system-ivory.vercel.app"],
     credentials: true,
   })
 );
