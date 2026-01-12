9a650989b5815ffecc8efae8da6d68f844cccea9
developedbyrd <developedbyrd@gmail.com>
2026-01-12 21:31:03 +0530

    Update API base URL to use environment variable for better configuration management

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BaseURL = import.meta.env.VITE_BASE_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${BaseURL}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
