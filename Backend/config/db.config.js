d0795b42b17bd5d288a723d1daec4de5a710664a
developedbyrd <developedbyrd@gmail.com>
2026-01-12 21:59:37 +0530

    Refactor MongoDB connection logic to remove retry mechanism and simplify error handling

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -1,26 +1,19 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "./config.js";
 
-const connectDB = async (retries = 5) => {
+const connectDB = async () => {
   const options = {
     maxPoolSize: 10,
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
   };
 
-  for (let i = 0; i < retries; i++) {
-    try {
-      await mongoose.connect(MONGO_URI, options);
-      console.log("MongoDB connected successfully");
-      return;
-    } catch (error) {
-      console.error(`Connection attempt ${i + 1} failed: ${error.message}`);
-      if (i === retries - 1) {
-        console.error("All connection attempts failed");
-        process.exit(1);
-      }
-      await new Promise((resolve) => setTimeout(resolve, 2000 * (i + 1)));
-    }
+  try {
+    await mongoose.connect(MONGO_URI, options);
+    console.log("MongoDB connected successfully");
+    return;
+  } catch (error) {
+    console.error(`Connection Error: ${error.message}`);
   }
 };
 
